ef696fa3cbf26f4226049190acf933e6ad7b6e5a
codecresta <danwgrace@gmail.com>
2015-04-01 17:56:23 +0100

    Added blank and null settings for most database fields.

--- flow/models.py
+++ flow/models.py
@@ -7,31 +7,31 @@ http://www.codecresta.com
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-from django.utils import timezone
+#from django.utils import timezone
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    picture = models.ImageField(upload_to=settings.USER_PICS_DIR, blank=True)
+    user = models.OneToOneField(User, blank=False, null=False)
+    picture = models.ImageField(upload_to=settings.USER_PICS_DIR, blank=True, null=True)
     def __str__(self):
         return self.user.username
 
 class Type(models.Model):
-    description = models.CharField(max_length=32)
+    description = models.CharField(max_length=32, blank=False, null=False)
     def __str__(self):
         return self.description
     
 class State(models.Model):
-    description = models.CharField(max_length=32)
+    description = models.CharField(max_length=32, blank=False, null=False)
     def __str__(self):
         return self.description
 
 class Flow(models.Model):
-    ref = models.CharField(max_length=32)
-    state = models.ForeignKey(State)
-    flow_type = models.ForeignKey(Type)
+    ref = models.CharField(max_length=32, blank=False, null=False)
+    state = models.ForeignKey(State, blank=False, null=False)
+    flow_type = models.ForeignKey(Type, blank=False, null=False)
     created = models.DateTimeField(db_index=True, auto_now_add=True)
     modified = models.DateTimeField(db_index=True, auto_now=True)
-    version = models.IntegerField()
+    version = models.IntegerField(blank=False, null=False)
     def __str__(self):
         return str(self.id)
     '''
@@ -45,29 +45,29 @@ class Flow(models.Model):
     '''
     
 class Event(models.Model):
-    user = models.ForeignKey(User)
-    flow = models.ForeignKey(Flow)
-    state = models.ForeignKey(State)
+    user = models.ForeignKey(User, blank=False, null=False)
+    flow = models.ForeignKey(Flow, blank=False, null=False)
+    state = models.ForeignKey(State, blank=False, null=False)
     created = models.DateTimeField(db_index=True, auto_now_add=True)
     def __str__(self):
         return str(self.id)
 
 class Log(models.Model):
-    user = models.ForeignKey(User)
-    flow = models.ForeignKey(Flow)
-    content = models.CharField(max_length=256)
+    user = models.ForeignKey(User, blank=False, null=False)
+    flow = models.ForeignKey(Flow, blank=False, null=False)
+    content = models.CharField(max_length=256, blank=False, null=False)
     created = models.DateTimeField(db_index=True, auto_now_add=True)
     def __str__(self):
         return str(self.id)
 
 class CustomA(models.Model):
-    event = models.ForeignKey(Event)
-    text = models.CharField(max_length=256)
+    event = models.ForeignKey(Event, blank=False, null=False)
+    text = models.CharField(max_length=256, blank=False, null=False)
     def __str__(self):
         return str(self.id)
 
 class CustomB(models.Model):
-    event = models.ForeignKey(Event)
-    integer = models.IntegerField()
+    event = models.ForeignKey(Event, blank=False, null=False)
+    integer = models.IntegerField(blank=False, null=False)
     def __str__(self):
         return str(self.id)
